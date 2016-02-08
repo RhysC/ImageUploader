0ff70099b5f23d5d3e7e9a2594fc66f51fa241d3
rhysc <rhysevancampbell@gmail.com>
2016-02-08 17:18:39 +0800

    Clean up

--- src/ImageUploader.Web/Controllers/ImagesController.cs
+++ src/ImageUploader.Web/Controllers/ImagesController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -12,20 +9,10 @@ namespace ImageUploader.Web.Controllers
 {
     public class ImagesController : ApiController
     {
-        // GET: api/Images
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Images/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Images
-        //public void Post([FromBody]string value)
+        /// <summary>
+        /// http://www.asp.net/web-api/overview/advanced/sending-html-form-data-part-2
+        /// </summary>
+        /// <returns></returns>
         public async Task<HttpResponseMessage> Post()
         {
             if (!Request.Content.IsMimeMultipartContent())
@@ -35,7 +22,7 @@ namespace ImageUploader.Web.Controllers
 
             string root = HttpContext.Current.Server.MapPath("~/App_Data");
             var provider = new MultipartFormDataStreamProvider(root);
-            
+
             try
             {
                 await Request.Content.ReadAsMultipartAsync(provider)
@@ -52,40 +39,25 @@ namespace ImageUploader.Web.Controllers
                             Trace.WriteLine(file.Headers.ContentDisposition.FileName);
                             Trace.WriteLine("Server file path: " + file.LocalFileName);
                         }
-                
+
                         // Show all the key-value pairs.
-                foreach (var key in provider.FormData.AllKeys)
+                        foreach (var key in provider.FormData.AllKeys)
                         {
                             foreach (var val in provider.FormData.GetValues(key))
                             {
                                 Trace.WriteLine(string.Format("{0}: {1}", key, val));
                             }
                         }
-
-
+                        
                         return Request.CreateResponse(HttpStatusCode.OK);
                     });
 
-
-
-               
                 return Request.CreateResponse(HttpStatusCode.OK);
             }
             catch (System.Exception e)
             {
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
             }
-
-        }
-
-        // PUT: api/Images/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Images/5
-        public void Delete(int id)
-        {
         }
     }
 }
